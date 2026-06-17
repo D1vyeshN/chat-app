30a4d3f7f0f9d9707427214bf44709560724bf4b
Divyesh Nandanwar <divyesh21j91@gmail.com>
2026-06-17 09:53:55 +0530

    setup ci and docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,8 +16,8 @@ const httpServer = createServer(app);
 initSocket(httpServer);
 
 // start listening
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT,"0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
